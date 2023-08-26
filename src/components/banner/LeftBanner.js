f0258ac0bd532bf4363accbb7212f6ff96ba8f01
sainath47 <reddysainath47@gmail.com>
2023-08-26 11:09:22 +0530

    changes what i have experience in

--- src/components/banner/LeftBanner.js
+++ src/components/banner/LeftBanner.js
@@ -4,7 +4,7 @@ import Media from './Media';
 
 const LeftBanner = () => {
     const [text] = useTypewriter({
-      words: ["Professional Coder.", "Full Stack Developer."],
+      words: ["Professional Coder.", "Full Stack Developer.", "Backend Developer.", "Node.js Developer.", "Frontend Developer."],
       loop: true,
       typeSpeed: 20,
       deleteSpeed: 10,
